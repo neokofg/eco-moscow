b348fbec227b36d0d365498740e1b7891a164a96
neoko <33611354+neokofg@users.noreply.github.com>
2024-09-18 14:57:38 +0300

    feat/comments-service

---
+++ eco-comments-service/app/Providers/Bindings/ServiceBindings.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Providers\Bindings;
+
+use Illuminate\Support\ServiceProvider;
+
+final class ServiceBindings extends ServiceProvider
+{
+    /**
+     * @return void
+     */
+    public function register(): void
+    {
+
+    }
+}
