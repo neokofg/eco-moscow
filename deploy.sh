3f147bd0197bf8459b9e28bccf17f833d6623878
neoko <33611354+neokofg@users.noreply.github.com>
2024-09-19 04:17:58 +0300

    fix/cors-n-deploy

--- deploy.sh
+++ deploy.sh
@@ -1,46 +1,54 @@
 #!/bin/sh
 
-CHANGES=$(git pull)
+git_output=$(git pull)
 
-if [[ $CHANGES == *"Already up to date."* ]]; then
+if [[ $git_output == *"Already up to date."* ]]; then
   echo "No changes detected. Exiting."
   exit 0
 fi
 
+CHANGES=$(git diff --name-only HEAD@{1} HEAD)
 
 declare -A services=(
-    ["backend"]="./eco-backend"
-    ["posts-service"]="./eco-posts-service"
-    ["comments-service"]="./eco-comments-service"
-    ["social-service"]="./eco-social-service"
-    ["events-service"]="./eco-events-service"
+    ["backend"]="eco-backend"
+    ["posts-service"]="eco-posts-service"
+    ["comments-service"]="eco-comments-service"
+    ["social-service"]="eco-social-service"
+    ["events-service"]="eco-events-service"
 )
 
 for service in "${!services[@]}"; do
-    CHANGES=$(git diff --name-only HEAD~1 HEAD)
+    service_dir="${services[$service]}"
+    echo "Processing $service ..."
 
-    if echo "$CHANGES" | grep -q 'composer.json'; then
-        echo "Running composer install for $service ..."
-        docker compose exec $service composer install
+    if echo "$CHANGES" | grep -q "^$service_dir/"; then
+            echo "Changes detected in $service"
+
+            if echo "$CHANGES" | grep -q "^$service_dir/composer.json"; then
+                echo "Running composer install for $service ..."
+                docker compose exec $service composer install
+            fi
+
+            if echo "$CHANGES" | grep -q "^$service_dir/database/migrations"; then
+                echo "Running migrations for $service ..."
+                docker compose exec $service php artisan migrate --force
+            fi
+
+            if echo "$CHANGES" | grep -q "^$service_dir/routes/api.php"; then
+                echo "Updating api for $service ..."
+                docker compose exec $service php artisan scribe:generate --force
+            fi
+
+            echo "Optimizing and caching routes for $service"
+            docker compose exec $service php artisan optimize:clear
+            docker compose exec $service php artisan route:cache
+
+            echo "Rebuilding and restarting $service container"
+            docker compose stop $service
+            docker compose up -d --build $service
+    else
+        echo "No changes detected in $service"
     fi
-
-    if echo "$CHANGES" | grep -q 'database/migrations'; then
-        echo "Running migrations for $service ..."
-        docker compose exec $service php artisan migrate --force
-    fi
-
-    if echo "$CHANGES" | grep -q 'routes/api.php'; then
-        echo "Updating api for $service ..."
-        docker compose exec $service php artisan scribe:generate
-    fi
-    echo "Optimizing and caching routes for $service"
-    docker compose exec $service php artisan optimize:clear
-    docker compose exec $service php artisan route:cache
-
-    echo "Rebuilding and restarting $service container"
-    docker compose stop $service
-    docker compose up -d --build $service
 done
 
 echo "All services updated and restarted."
-exit 0
